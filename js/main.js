f5997c0aa5d326d1f0347f2302225eb907144c66
proyectos <emersongmz01@gmail.com>
2026-05-30 21:41:06 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    // Animación interactiva para el menú desplegable (Dropdown)
+   
     $('.dropdown-toggle').click(function(e) {
-        e.preventDefault(); // Evita que recargue la página al hacer clic
+        e.preventDefault(); 
         $(this).next('.dropdown-menu').slideToggle(300);
     });
 
-    // Cerrar el menú si se hace clic fuera de él
+    
     $(document).click(function(e) {
         var target = e.target;
         if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown')) {
@@ -62,11 +62,6 @@ break;
 respuestaBot.innerHTML =
 `<p style="margin-top:15px;"><b>TechBot:</b> ${texto}</p>`;
 
-
-
-chatBody.scrollTop = chatBody.scrollHeight;
-
-
 }
 document.getElementById("cerrarChat").onclick = () => {
 
@@ -85,3 +80,68 @@ msg.style.display = "none";
 }
 
 }, 5000);
+
+const cards =
+document.querySelectorAll(".tarjeta-intro");
+
+cards.forEach(card => {
+
+let rotX = 0;
+let rotY = 0;
+
+let velX = 0;
+let velY = 0;
+
+card.addEventListener("mousemove", (e) => {
+
+let rect =
+card.getBoundingClientRect();
+
+let x =
+e.clientX - rect.left;
+
+let y =
+e.clientY - rect.top;
+
+let centerX =
+rect.width / 2;
+
+let centerY =
+rect.height / 2;
+
+velY =
+(x - centerX) / 20;
+
+velX =
+-(y - centerY) / 20;
+
+});
+
+card.addEventListener("mouseleave", () => {
+
+velX = 0;
+velY = 0;
+
+});
+
+function animate() {
+
+rotX +=
+(velX - rotX) * 0.1;
+
+rotY +=
+(velY - rotY) * 0.1;
+
+card.style.transform =
+
+`perspective(1000px)
+rotateX(${rotX}deg)
+rotateY(${rotY}deg)`;
+
+requestAnimationFrame(animate);
+
+}
+
+animate();
+
+});
